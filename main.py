c8faaaa4b4829dca370221ac651ff8454ef113ea
zy <2075221336@qq.com>
2022-11-16 21:36:46 +0800

    更新的主窗口页面

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import threading
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QLabel, QListWidget
 import sys
+
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QTableView, QTableWidget
+
 import MusicPlayer
 from gui import main_ui  # 导入ui文件
 
@@ -13,7 +18,16 @@ class Main_window(QtWidgets.QMainWindow, main_ui.Ui_MainWindow):
         self.ini_window()
 
     def ini_window(self):
-        self.main_layout.addWidget(self.music)
+        self.music.playlist_button.clicked.connect(self.playlist)
+        self.bottom_layout.addWidget(self.music)
+        self.music.show()
+
+    def playlist(self):
+        self.music_lst_widget = QTableWidget()
+        self.music_lst_widget.setCellWidget(0,0,QLabel("111"))
+        self.music_lst_widget.move(self.music.playlist_button.x() - 20, self.music.playlist_button.y() - 20)
+
+        print(self.music_lst_widget.x(), self.music_lst_widget.y())
 
 
 if __name__ == "__main__":
