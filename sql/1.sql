17920574f8e881258706cf73549c630bc6b79730
Blackteaxx <211820073@smail.nju.edu.cn>
2022-11-22 16:38:11 +0800

    11.22 更改数据库字段

--- sql/1.sql
+++ sql/1.sql
@@ -1,3 +1,3 @@
 insert into Label values('111')
-insert into UserInfo values(1,'nju','1','这里是南哪儿大学','1902-05-20','1','1')
 insert into Account_Password(Account,Password) values ('21182','5151')
+insert into UserInfo values(1,'nju','1','这里是南哪儿大学','1902-05-20','1','1')
