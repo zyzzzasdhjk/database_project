e4193d8d008bb92e44bc33dd9d87c58f6ca63a0a
Blackteaxx <211820073@smail.nju.edu.cn>
2022-11-23 21:57:06 +0800

    11.23

--- login_and_register/register_Panel.py
+++ login_and_register/register_Panel.py
@@ -52,11 +52,10 @@ class RegisterPanel(QWidget, Ui_register_window):
 
     def check_register(self):
         print('register')
-        if self.register_password_text == self.register_password2_text and self.register_password_text != '':
+        if self.register_password_text.text() == self.register_password2_text.text() and self.register_password_text != '':
             self.check_register_signal.emit(self.register_username_text.text(), self.register_password_text.text())
         else:
             QMessageBox.about(self, 'about', "密码输入不一致，请重试")
-
 
 
 if __name__ == '__main__':
