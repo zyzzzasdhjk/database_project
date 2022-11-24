2344f71cb0fd3e1284bab1511f0c4258c8ccf741
Blackteaxx <211820073@smail.nju.edu.cn>
2022-11-25 00:17:17 +0800

    11.24 更改获取搜索用户界面和别人个人资料显示界面，变得可以跳转

--- app/OtherShow_Panel.py
+++ app/OtherShow_Panel.py
@@ -1,25 +1,110 @@
 from PyQt5.Qt import *
 from gui.OtherShow import Ui_OtherShow
+import datetime
 
 
-class Window(QWidget, Ui_OtherShow):
+class MyButtonDelegate(QItemDelegate):
+    """创建按钮代理"""
+
     def __init__(self, parent=None):
-        # 调用父类的初始化方法
+        super(MyButtonDelegate, self).__init__(parent)
+
+    def paint(self, painter, option, index):
+        if not self.parent().indexWidget(index):
+            button_read = QPushButton(
+                self.tr('进入'),
+                self.parent(),
+                clicked=self.parent().enterSheet
+            )
+
+            button_read.index = index.row()
+
+            h_box_layout = QHBoxLayout()
+            h_box_layout.addWidget(button_read)
+
+            h_box_layout.setContentsMargins(0, 0, 0, 0)
+            h_box_layout.setAlignment(Qt.AlignCenter)
+            widget = QWidget()
+            widget.setLayout(h_box_layout)
+            self.parent().setIndexWidget(
+                index,
+                widget
+            )
+
+
+class MyTableView(QTableView):
+    """创建音乐表视图"""
+    enterSheetsignal = pyqtSignal(int)
+
+    def __init__(self, parent=None, lst=None):
+        super(MyTableView, self).__init__(parent)
+        self.setStyleSheet('font: 10pt "微软雅黑";')
+
+        self.setOtherShowTableView(lst)
+        # 设置按钮代理
+        self.setItemDelegateForColumn(3, MyButtonDelegate(self))
+        # 设置不可选中
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+    def getData(self, lst):
+        """设置tableview的模型
+            接受歌单的三项属性二维列表
+            [[SID,SName,MusicNum]]"""
+
+        self.model = QStandardItemModel(0, 0)
+        self.Headers = ['ID', '歌单名', '歌曲数目', '操作']
+        self.model.setHorizontalHeaderLabels(self.Headers)
+        rowNum = len(lst)
+        for row in range(rowNum):
+            for column in range(3):
+                item = QStandardItem(f'{lst[row][column]}')
+                self.model.setItem(row, column, item)
+
+    def setOtherShowTableView(self, lst):
+        self.getData(lst)
+        self.setModel(self.model)
+        # 自适应布局，设置高度与宽度
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.verticalHeader().setDefaultSectionSize(40)
+        self.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+
+    def enterSheet(self):
+        """进入按钮的信号
+            发出：SID"""
+        index = self.model.index(self.sender().index, 0)
+        data = int(self.model.data(index))
+        print(data)
+        self.enterSheetsignal.emit(data)
+
+
+class OtherShowPanel(QWidget, Ui_OtherShow):
+
+    def __init__(self, parent=None, lst=[]):
         super().__init__(parent)
         self.setupUi(self)
-        self.setPlaylist()
+        self.setOtherShowTableView(lst)
 
+    def loadOtherInfo(self, lst):
+        """加载个人数据
+            传入个人信息数据列表
+            [UID, UName, USex, UIntro, UBirthday, UIsVip, LText]"""
+        self.ID = lst[0]
+        self.OtherShowUNameLabel.setText(lst[1])
+        self.OtherShowUSexLabel.setText(lst[2])
+        self.OtherShowUIntroLabel.setPlainText(lst[3])
+        self.OtherShowUBirthdayLabel.setText(lst[4])
+        if lst[5] == '1':
+            self.OtherShowUIsVIPLabel.setText(lst[5])
+        self.OtherShowULabelLabel.setText(lst[6])
 
-    def setPlaylist(self):
-        self.model = QStandardItemModel()
-        self.Headers = ['ID', '操作', '标题', '创建人']
-        self.model.setHorizontalHeaderLabels(self.Headers)
-        self.model.appendRow(QStandardItem('1'))
-        self.OtherShowPlayListtableView.setModel(self.model)
-        # 自适应布局，设置高度与宽度
-        self.OtherShowPlayListtableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.OtherShowPlayListtableView.verticalHeader().setDefaultSectionSize(40)
-        self.OtherShowPlayListtableView.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+    def setOtherShowTableView(self, lst):
+        self.OtherShowTableView = MyTableView(lst=lst)
+        self.OtherShowDownLayout.addWidget(self.OtherShowTableView)
+
+    def loadOtherShowTableViewModel(self, lst):
+        """接收二维列表
+        [[SID,SName,MusicNum]]"""
+        self.OtherShowTableView.setOtherShowTableView(lst)
 
 
 if __name__ == '__main__':
@@ -30,6 +115,6 @@ if __name__ == '__main__':
     # setup stylesheet
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
-    window = Window()
+    window = OtherShowPanel(lst=[[1, 1, 1]])
     window.show()
     sys.exit(app.exec_())
--- app/getUserSearchPanel.py
+++ app/getUserSearchPanel.py
@@ -2,34 +2,95 @@ from PyQt5.Qt import *
 from gui.getUserSearch import Ui_getUserSearch
 import datetime
 
-class getUserSearchPanel(QWidget, Ui_getUserSearch):
-    def __init__(self, parent=None, UserData=[]):
-        # 调用父类的初始化方法
-        super().__init__(parent)
-        self.setupUi(self)
-        self.iniTableview(UserData)
 
-    def iniTableview(self, UserData):
+class MyButtonDelegate(QItemDelegate):
+    """创建按钮代理"""
+
+    def __init__(self, parent=None):
+        super(MyButtonDelegate, self).__init__(parent)
+
+    def paint(self, painter, option, index):
+        if not self.parent().indexWidget(index):
+            button_read = QPushButton(
+                self.tr('查看'),
+                self.parent(),
+                clicked=self.parent().enterUser
+            )
+
+            button_read.index = index.row()
+
+            h_box_layout = QHBoxLayout()
+            h_box_layout.addWidget(button_read)
+
+            h_box_layout.setContentsMargins(0, 0, 0, 0)
+            h_box_layout.setAlignment(Qt.AlignCenter)
+            widget = QWidget()
+            widget.setLayout(h_box_layout)
+            self.parent().setIndexWidget(
+                index,
+                widget
+            )
+
+
+class MyTableView(QTableView):
+    """创建音乐表视图"""
+    enterUsersignal = pyqtSignal(int)
+
+    def __init__(self, parent=None, lst=None):
+        super(MyTableView, self).__init__(parent)
+        self.setStyleSheet('font: 10pt "微软雅黑";')
+
+        self.setUserSearchTableView(lst)
+        # 设置按钮代理
+        self.setItemDelegateForColumn(3, MyButtonDelegate(self))
+        # 设置不可选中
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+    def getData(self, lst):
         """接受一个二维列表
         [[UID, UName, USex]]"""
-
         self.model = QStandardItemModel(0, 0)
-        self.Headers = ['ID', '昵称', '性别']
+        self.Headers = ['ID', '昵称', '性别', '操作']
         self.model.setHorizontalHeaderLabels(self.Headers)
-        rowNum = len(UserData)
+        rowNum = len(lst)
         columnNum = 3
         for row in range(rowNum):
             for column in range(columnNum):
-                item = QStandardItem(str(UserData[row][column]))
+                item = QStandardItem(str(lst[row][column]))
                 self.model.setItem(row, column, item)
 
-        self.getUserSearchTableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+    def setUserSearchTableView(self, lst):
+        self.getData(lst)
+        self.setModel(self.model)
         # 自适应布局，设置高度与宽度
-        self.getUserSearchTableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.getUserSearchTableView.verticalHeader().setDefaultSectionSize(40)
-        self.getUserSearchTableView.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.verticalHeader().setDefaultSectionSize(40)
+        self.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
 
-        self.getUserSearchTableView.setModel(self.model)
+    def enterUser(self):
+        """进入按钮的信号
+            发出：UID"""
+        index = self.model.index(self.sender().index, 0)
+        data = int(self.model.data(index))
+        print(data)
+        self.enterUsersignal.emit(data)
+
+
+class getUserSearchPanel(QWidget, Ui_getUserSearch):
+
+    def __init__(self, parent=None, lst=[]):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setUserSearchTableView(lst)
+
+    def setUserSearchTableView(self, lst):
+        self.getUserSearchTableView = MyTableView(lst=lst)
+        self.getUserSearchDownLayout.addWidget(self.getUserSearchTableView)
+
+    def loadUserSearchTableViewModel(self, lst):
+        """接收二维列表
+        [[UID, UName, USex]]"""
+        self.getUserSearchTableView.setUserSearchTableView(lst)
 
 
 if __name__ == '__main__':
@@ -41,6 +102,6 @@ if __name__ == '__main__':
 
     # setup stylesheet
     qt_material.apply_stylesheet(app, theme='dark_teal.xml')
-    window = getUserSearchPanel(UserData=[[1, '红茶honer', '男']])
+    window = getUserSearchPanel(lst=[[1, '红茶honer', '男']])
     window.show()
     sys.exit(app.exec_())
